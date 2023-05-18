18071bd1dcc6f2444aa66a285a6c31af99f0b045
sha-mo-xuan-shen <2052039701@qq.com>
2023-05-18 14:10:01 +0800

    小补充

--- src/main/java/com/ssmp/dao/BookDao.java
+++ src/main/java/com/ssmp/dao/BookDao.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface BookDao {
 
-    @Select("select * from tbl_book where id=#{id}")
+    @Select("select * from tbl_book where id=#{id}")//别忘了#
     Book getById(Integer id);
 }
