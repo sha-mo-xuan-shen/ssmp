3a35742bf0fd381b40f01ac3eb9af99ff4fba02f
sha-mo-xuan-shen <2052039701@qq.com>
2023-05-25 11:09:11 +0800

    static文件，以后别再乱找了

--- src/main/java/com/ssmp/service/impl/IBookServiceImpl.java
+++ src/main/java/com/ssmp/service/impl/IBookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssmp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +18,10 @@ public class IBookServiceImpl extends ServiceImpl<BookDao,Book> implements IBook
     @Override
     public IPage<Book> getPage(int currentPage, int pageSize) {
         IPage<Book> page = new Page<>(currentPage,pageSize);
+        QueryWrapper qw = new QueryWrapper();
+        qw.like("type","程序");
+        LambdaQueryWrapper<Book> lqw = new LambdaQueryWrapper<>();
+        lqw.like(Book::getDescription,"真");
         bookDao.selectPage(page,null);
         return page;
     }
